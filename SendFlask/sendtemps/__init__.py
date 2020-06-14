2482d2694e9f21ef576152745fb4db1627198978
Ben Rhodeland <benplusnumber@gmail.com>
2020-06-14 15:16:30 -0700

    init

---
+++ SendFlask/sendtemps/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+app = Flask(__name__)
+Bootstrap(app)
+
+app.config.from_pyfile('default.cfg')
+app.config.from_envvar('SNOTELIER_CONFIG', silent=True)
+
+from sendtemps import temps
